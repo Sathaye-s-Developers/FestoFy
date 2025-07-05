52ae6a3c934c7e083d7f3b2560358c2c94a14822
Kushagra Ghadigaonkar <149246936+Kushagra-ghadigaonkar@users.noreply.github.com>
2025-07-05 20:33:59 +0530

    changing some codes

--- backend/app.js
+++ backend/app.js
@@ -23,17 +23,7 @@ connectDB();
 
 // Middleware
 
-// const corsOptions = {
-//   origin: process.env.Frontend_Url,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// };
-
-
-
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions)); // ✅ same config
+app.use(cors({origin:process.env.Frontend_Url}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
