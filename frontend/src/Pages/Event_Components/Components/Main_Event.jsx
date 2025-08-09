e6d2b72d7f04559f22170adac81dbc035ee34f5e
Kushagra Ghadigaonkar <149246936+Kushagra-ghadigaonkar@users.noreply.github.com>
2025-08-09 18:46:36 +0530

    minor changes of colleg name

--- frontend/src/Pages/Event_Components/Components/Main_Event.jsx
+++ frontend/src/Pages/Event_Components/Components/Main_Event.jsx
@@ -9,7 +9,7 @@ const Main_Event = () => {
   return (
     <div className='mt-5'>
       <div>
-        <p className='mt-10 text-center md:text-5xl text-4xl bg-gradient-to-r from-cyan-700 via-teal-500 to-white font-serif bg-clip-text text-transparent font-bold leading-tight'>Welcome To {college.short_name}'s Hub</p>
+        <p className='mt-10 text-center md:text-5xl text-4xl bg-gradient-to-r from-cyan-700 via-teal-500 to-white font-serif bg-clip-text text-transparent font-bold leading-tight'>Welcome To {college?college.short_name:"Your College"}'s Hub</p>
         <p className='pl-3 mt-5 text-2xl text-white text-center'>Experience, Engage, Excel – College Events</p>
         {/* <p className='text-white text-center text-5xl'>No Events</p> */}
       </div>
